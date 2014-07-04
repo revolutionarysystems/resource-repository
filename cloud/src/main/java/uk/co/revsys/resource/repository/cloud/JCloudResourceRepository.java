17a4f7652df8ef64fccdf61d8cd9e5683c808679
Adam Knight <adam.knight@opensquare.co.uk>
2014-07-04 14:37:58 +0100

    Allowed a base directory to be set for jcloud repositories

--- cloud/src/main/java/uk/co/revsys/resource/repository/cloud/JCloudResourceRepository.java
+++ cloud/src/main/java/uk/co/revsys/resource/repository/cloud/JCloudResourceRepository.java
@@ -20,33 +20,44 @@ public class JCloudResourceRepository implements ResourceRepository {
 
 	private final BlobStore blobStore;
 	private final String container;
+    private final String baseDir;
 
 	public JCloudResourceRepository(BlobStore blobStore, String container) {
 		this.blobStore = blobStore;
 		this.container = container;
+        this.baseDir = "";
 	}
+
+    public JCloudResourceRepository(BlobStore blobStore, String container, String baseDir) {
+        this.blobStore = blobStore;
+        this.container = container;
+        if(!baseDir.endsWith("/")){
+            baseDir = baseDir + "/";
+        }
+        this.baseDir = baseDir;
+    }
 
 	@Override
 	public void write(Resource resource, final InputStream inputStream) throws IOException {
 		byte[] content = IOUtils.toByteArray(inputStream);
-		Blob blob = blobStore.blobBuilder(resource.getPath() + "/" + resource.getName()).payload(content).contentLength(content.length).build();
+		Blob blob = blobStore.blobBuilder(baseDir + resource.getPath() + "/" + resource.getName()).payload(content).contentLength(content.length).build();
 		blobStore.putBlob(container, blob);
 	}
 
 	@Override
 	public InputStream read(Resource resource) throws IOException {
-		Blob blob = blobStore.getBlob(container, resource.getPath() + "/" + resource.getName());
+		Blob blob = blobStore.getBlob(container, baseDir + resource.getPath() + "/" + resource.getName());
 		return blob.getPayload().getInput();
 	}
 
 	@Override
 	public void delete(Resource resource) throws IOException {
-		blobStore.removeBlob(container, resource.getPath() + "/" + resource.getName());
+		blobStore.removeBlob(container, baseDir + resource.getPath() + "/" + resource.getName());
 	}
 
 	@Override
 	public void delete(Directory directory) throws IOException {
-		blobStore.clearContainer(container, new ListContainerOptions().inDirectory(directory.getPath() + "/" + directory.getName()).recursive());
+		blobStore.clearContainer(container, new ListContainerOptions().inDirectory(baseDir + directory.getPath() + "/" + directory.getName()).recursive());
 	}
 
 	@Override
@@ -54,7 +65,7 @@ public class JCloudResourceRepository implements ResourceRepository {
 		List<RepositoryItem> resources = new LinkedList<RepositoryItem>();
 		ListContainerOptions listContainerOptions = new ListContainerOptions();
 		if (!path.isEmpty()) {
-			listContainerOptions.inDirectory(path);
+			listContainerOptions.inDirectory(baseDir + path);
 		};
 		PageSet<? extends StorageMetadata> list = blobStore.list(container, listContainerOptions);
 		for (StorageMetadata item : list) {
@@ -80,7 +91,7 @@ public class JCloudResourceRepository implements ResourceRepository {
 		List<Directory> directories = new LinkedList<Directory>();
 		ListContainerOptions listContainerOptions = new ListContainerOptions();
 		if (!path.isEmpty()) {
-			listContainerOptions.inDirectory(path);
+			listContainerOptions.inDirectory(baseDir + path);
 		};
 		PageSet<? extends StorageMetadata> list = blobStore.list(container, listContainerOptions);
 		for (StorageMetadata item : list) {
@@ -100,7 +111,7 @@ public class JCloudResourceRepository implements ResourceRepository {
 		List<Resource> resources = new LinkedList<Resource>();
 		ListContainerOptions listContainerOptions = new ListContainerOptions();
 		if (!path.isEmpty()) {
-			listContainerOptions.inDirectory(path);
+			listContainerOptions.inDirectory(baseDir + path);
 		};
 		PageSet<? extends StorageMetadata> list = blobStore.list(container, listContainerOptions);
 		for (StorageMetadata item : list) {
--- cloud/src/main/java/uk/co/revsys/resource/repository/cloud/JCloudResourceRepositoryBuilder.java
+++ cloud/src/main/java/uk/co/revsys/resource/repository/cloud/JCloudResourceRepositoryBuilder.java
@@ -12,18 +12,28 @@ public class JCloudResourceRepositoryBuilder implements ResourceRepositoryBuilde
     private final String identity;
     private final String credential;
     private final String container;
+    private final String baseDir;
 
     public JCloudResourceRepositoryBuilder(String type, String identity, String credential, String container) {
         this.type = type;
         this.identity = identity;
         this.credential = credential;
         this.container = container;
+        this.baseDir = "";
+    }
+
+    public JCloudResourceRepositoryBuilder(String type, String identity, String credential, String container, String baseDir) {
+        this.type = type;
+        this.identity = identity;
+        this.credential = credential;
+        this.container = container;
+        this.baseDir = baseDir;
     }
     
     @Override
     public ResourceRepository build() {
         BlobStore blobStore = ContextBuilder.newBuilder(type).credentials(identity, credential).buildView(BlobStoreContext.class).getBlobStore();
-        return new JCloudResourceRepository(blobStore, container);
+        return new JCloudResourceRepository(blobStore, container, baseDir);
     }
 
 }
