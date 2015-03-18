277b44ea53c3c34773ec58683373414e2d381330
Adam Knight <adam@revolutionarysystems.co.uk>
2015-03-18 13:53:48 +0000

    Fixed deleting directories

--- service/src/main/java/uk/co/revsys/resource/repository/service/ResourceRepositoryServlet.java
+++ service/src/main/java/uk/co/revsys/resource/repository/service/ResourceRepositoryServlet.java
@@ -29,110 +29,124 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class ResourceRepositoryServlet extends HttpServlet {
 
-	private ResourceRepository resourceRepository;
-	private JadeConfiguration jadeConfig;
-	private DiskFileItemFactory factory;
-	private ServletFileUpload upload;
+    private ResourceRepository resourceRepository;
+    private JadeConfiguration jadeConfig;
+    private DiskFileItemFactory factory;
+    private ServletFileUpload upload;
 
-	@Override
-	public void init() throws ServletException {
-		WebApplicationContext webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
-		this.resourceRepository = webApplicationContext.getBean(ResourceRepository.class);
-		//this.viewResolver = webApplicationContext.getBean(JadeViewResolver.class);
+    @Override
+    public void init() throws ServletException {
+        WebApplicationContext webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+        this.resourceRepository = webApplicationContext.getBean(ResourceRepository.class);
+        //this.viewResolver = webApplicationContext.getBean(JadeViewResolver.class);
         jadeConfig = new JadeConfiguration();
         jadeConfig.setTemplateLoader(new ClasspathTemplateLoader());
         jadeConfig.setCaching(false);
         jadeConfig.setPrettyPrint(false);
-		this.factory = new DiskFileItemFactory();
-		this.upload = new ServletFileUpload(factory);
-	}
+        this.factory = new DiskFileItemFactory();
+        this.upload = new ServletFileUpload(factory);
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		try {
-			resp.setHeader("Access-Control-Allow-Origin", "*");
-			String filePath = req.getRequestURI().substring((req.getContextPath() + req.getServletPath()).length() + 1);
-			if (filePath.isEmpty() || filePath.endsWith("/")) {
-				List<Directory> directories = resourceRepository.listDirectories(filePath);
-				List<Resource> resources = resourceRepository.listResources(filePath);
-				Map<String, Object> model = new HashMap<String, Object>();
-				model.put("directories", directories);
-				model.put("resources", resources);
-				String viewType = req.getParameter("view");
-				if (viewType != null && viewType.equals("json")) {
-					resp.getWriter().write(new JSONObject(model).toString());
-					resp.setContentType(MediaType.APPLICATION_JSON.toString());
-				} else {
-					String parentPath = filePath;
-					if (!parentPath.isEmpty()) {
-						parentPath = parentPath.substring(0, parentPath.length() - 1);
-						parentPath = req.getContextPath() + "/" + parentPath.substring(0, parentPath.lastIndexOf("/") + 1);
-					} else {
-						parentPath = req.getContextPath() + "/";
-					}
-					model.put("parentPath", parentPath);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            resp.setHeader("Access-Control-Allow-Origin", "*");
+            String filePath = req.getRequestURI().substring((req.getContextPath() + req.getServletPath()).length() + 1);
+            if (filePath.isEmpty() || filePath.endsWith("/")) {
+                List<Directory> directories = resourceRepository.listDirectories(filePath);
+                List<Resource> resources = resourceRepository.listResources(filePath);
+                Map<String, Object> model = new HashMap<String, Object>();
+                model.put("directories", directories);
+                model.put("resources", resources);
+                String viewType = req.getParameter("view");
+                if (viewType != null && viewType.equals("json")) {
+                    resp.getWriter().write(new JSONObject(model).toString());
+                    resp.setContentType(MediaType.APPLICATION_JSON.toString());
+                } else {
+                    String parentPath = filePath;
+                    if (!parentPath.isEmpty()) {
+                        parentPath = parentPath.substring(0, parentPath.length() - 1);
+                        parentPath = req.getContextPath() + "/" + parentPath.substring(0, parentPath.lastIndexOf("/") + 1);
+                    } else {
+                        parentPath = req.getContextPath() + "/";
+                    }
+                    model.put("parentPath", parentPath);
                     JadeTemplate template = jadeConfig.getTemplate("views/resources.jade");
                     jadeConfig.renderTemplate(template, model, resp.getWriter());
-				}
-			} else {
-				String resourcePath = filePath.substring(0, filePath.lastIndexOf("/"));
-				String resourceName = filePath.substring(filePath.lastIndexOf("/") + 1);
-				InputStream resourceStream = resourceRepository.read(new Resource(resourcePath, resourceName));
-				IOUtils.copy(resourceStream, resp.getOutputStream());
-			}
-		} catch (FileNotFoundException ex) {
+                }
+            } else {
+                String resourcePath = filePath.substring(0, filePath.lastIndexOf("/"));
+                String resourceName = filePath.substring(filePath.lastIndexOf("/") + 1);
+                InputStream resourceStream = resourceRepository.read(new Resource(resourcePath, resourceName));
+                IOUtils.copy(resourceStream, resp.getOutputStream());
+            }
+        } catch (FileNotFoundException ex) {
             ex.printStackTrace();
-			resp.sendError(404);
-		} catch (Exception ex) {
-			throw new ServletException(ex);
-		}
-	}
+            resp.sendError(404);
+        } catch (Exception ex) {
+            throw new ServletException(ex);
+        }
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		try {
-			resp.setHeader("Access-Control-Allow-Origin", "*");
-			String filePath = req.getRequestURI().substring((req.getContextPath() + req.getServletPath()).length() + 1);
-			List<FileItem> items = upload.parseRequest(req);
-			for (FileItem item : items) {
-				if (!item.isFormField()) {
-					String resourcePath;
-					String resourceName;
-					if (filePath.endsWith("/")) {
-						resourcePath = filePath.substring(0, filePath.lastIndexOf("/"));
-						resourceName = item.getName();
-					} else {
-						resourcePath = filePath.substring(0, filePath.lastIndexOf("/"));
-						resourceName = filePath.substring(filePath.lastIndexOf("/") + 1);
-					}
-					Resource resource = new Resource(resourcePath, resourceName);
-					InputStream uploadStream = item.getInputStream();
-					resourceRepository.write(resource, uploadStream);
-					uploadStream.close();
-					resp.getWriter().write(resource.getName() + " uploaded successfully");
-				}
-			}
-		} catch (FileUploadException ex) {
-			throw new ServletException(ex);
-		}
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            resp.setHeader("Access-Control-Allow-Origin", "*");
+            String filePath = req.getRequestURI().substring((req.getContextPath() + req.getServletPath()).length() + 1);
+            List<FileItem> items = upload.parseRequest(req);
+            for (FileItem item : items) {
+                if (!item.isFormField()) {
+                    String resourcePath;
+                    String resourceName;
+                    if (filePath.endsWith("/")) {
+                        resourcePath = filePath.substring(0, filePath.lastIndexOf("/"));
+                        resourceName = item.getName();
+                    } else {
+                        resourcePath = filePath.substring(0, filePath.lastIndexOf("/"));
+                        resourceName = filePath.substring(filePath.lastIndexOf("/") + 1);
+                    }
+                    Resource resource = new Resource(resourcePath, resourceName);
+                    InputStream uploadStream = item.getInputStream();
+                    resourceRepository.write(resource, uploadStream);
+                    uploadStream.close();
+                    resp.getWriter().write(resource.getName() + " uploaded successfully");
+                }
+            }
+        } catch (FileUploadException ex) {
+            throw new ServletException(ex);
+        }
+    }
 
-	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("doDelete");
-		resp.setHeader("Access-Control-Allow-Origin", "*");
-		String filePath = req.getRequestURI().substring((req.getContextPath() + req.getServletPath()).length() + 1);
-		String resourcePath = filePath.substring(0, filePath.lastIndexOf("/"));
-		String resourceName = filePath.substring(filePath.lastIndexOf("/") + 1);
-		resourceRepository.delete(new Resource(resourcePath, resourceName));
-		resp.getWriter().write(resourceName + " deleted successfully");
-	}
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doDelete");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        String filePath = req.getRequestURI().substring((req.getContextPath() + req.getServletPath()).length() + 1);
+        if (filePath.endsWith("/")) {
+            filePath = filePath.substring(0, filePath.length()-1);
+            Directory directory;
+            if(filePath.contains("/")){
+                String dirPath = filePath.substring(0, filePath.lastIndexOf("/"));
+                String dirName = filePath.substring(filePath.lastIndexOf("/")+1);
+                directory = new Directory(dirPath, dirName);
+            }else{
+                directory = new Directory(filePath);
+            }
+            resourceRepository.delete(directory);
+            resp.getWriter().write(directory.getName() + " deleted successfully");
+        } else {
+            String resourcePath = filePath.substring(0, filePath.lastIndexOf("/"));
+            String resourceName = filePath.substring(filePath.lastIndexOf("/") + 1);
+            resourceRepository.delete(new Resource(resourcePath, resourceName));
+            resp.getWriter().write(resourceName + " deleted successfully");
+        }
+    }
 
-	@Override
-	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setHeader("Access-Control-Allow-Origin", "*");
-		resp.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-		super.doOptions(req, resp);
-	}
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        super.doOptions(req, resp);
+    }
 
 }
